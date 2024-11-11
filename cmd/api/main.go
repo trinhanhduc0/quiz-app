89997644eb6a7b0244c103158a4a1e665c7333b2
trinhanhduc0 <155358378+trinhanhduc0@users.noreply.github.com>
2024-11-11 19:08:10 +0700

    Update main.go

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -11,22 +11,22 @@ import (
 
 func LoadConfig() bool {
 	// Không cần dòng này khi triển khai trên Fly.io
-	err := godotenv.Load(".env")
-	if err != nil {
+	// err := godotenv.Load(".env")
+	// if err != nil {
 
-	port := os.Getenv("PORT")
-	appEnv := os.Getenv("APP_ENV")
-	redisURI := os.Getenv("REDIS_URI")
-	mongoDBURI := os.Getenv("MONGODB_URI")
-	awsAccessKeyID := os.Getenv("aws_access_key_id")
-	awsSecretAccessKey := os.Getenv("aws_secret_access_key")
+	// port := os.Getenv("PORT")
+	// appEnv := os.Getenv("APP_ENV")
+	// redisURI := os.Getenv("REDIS_URI")
+	// mongoDBURI := os.Getenv("MONGODB_URI")
+	// awsAccessKeyID := os.Getenv("aws_access_key_id")
+	// awsSecretAccessKey := os.Getenv("aws_secret_access_key")
 
-	// Kiểm tra nếu các biến môi trường quan trọng có giá trị hay không
-	if port == "" || appEnv == "" || redisURI == "" || mongoDBURI == "" || awsAccessKeyID == "" || awsSecretAccessKey == "" {
-		fmt.Println(port)
-		log.Fatal("One or more environment variables are not set.")
-		return false
-	}
+	// // Kiểm tra nếu các biến môi trường quan trọng có giá trị hay không
+	// if port == "" || appEnv == "" || redisURI == "" || mongoDBURI == "" || awsAccessKeyID == "" || awsSecretAccessKey == "" {
+	// 	fmt.Println(port)
+	// 	log.Fatal("One or more environment variables are not set.")
+	// 	return false
+	// }
 	
 	return true
 }
